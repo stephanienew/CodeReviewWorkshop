03d80b0e111f1d7a3e324eea4cbd4301a9f4f473
Stephanie New <stephanie@deliverr.com>
2018-06-12 23:05:28 +0000

    Code samples for high level feedback

---
+++ design.ts
@@ -0,0 +1,81 @@
+enum UserState {
+  ACTIVE = "ACTIVE",
+  FROZEN = "FROZEN",
+  DELETED = "DELETED",
+}
+
+enum UserAction {
+  CREATE_FOLDER = "CREATE_FOLDER",
+  SEND_SPAM = "SEND_SPAM",
+  DELETE_ACCOUNT = "DELETE_ACCOUNT",
+}
+
+/**
+ * ACTIVE -- CREATE FOLDER --> ACTIVE
+ * 
+ * ACTIVE -- SEND SPAM --> FROZEN
+ * 
+ * ACTIVE -- DELETE ACCOUNT --> DELETED
+ * 
+ */
+const getNextState = (currentUserState: UserState, action: UserAction): UserState => {
+  if (currentUserState === UserState.ACTIVE && action === UserAction.CREATE_FOLDER) {
+    return UserState.ACTIVE;
+  }
+  
+  if (currentUserState === UserState.ACTIVE && action === UserAction.SEND_SPAM) {
+    return UserState.FROZEN;
+  }
+
+  if (currentUserState === UserState.ACTIVE && action === UserAction.DELETE_ACCOUNT) {
+    return UserState.DELETED;
+  }
+  
+  throw new Error("invalid transition");
+};
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+interface StateMachine {
+  state: UserState;
+  handleUserAction(action: UserAction);
+}
+
+class UserStateMachine implements StateMachine {
+  state: UserState;
+  
+  constructor(startingState: UserState) {
+    this.state = startingState;
+  }
+
+  handleUserAction(action: UserAction) {
+    if (this.state === UserState.ACTIVE && action === UserAction.CREATE_FOLDER) {
+      return UserState.ACTIVE;
+    }
+    
+    if (this.state === UserState.ACTIVE && action === UserAction.SEND_SPAM) {
+      return UserState.FROZEN;
+    }
+  
+    if (this.state === UserState.ACTIVE && action === UserAction.DELETE_ACCOUNT) {
+      return UserState.DELETED;
+    }
+
+    throw new Error("invalid")
+  }
+}
+
+class TeamUserStateMachine implements StateMachine {
+  ...
+}
+
+class AdminStateMachine implements StateMachine {
+  ...
+}
